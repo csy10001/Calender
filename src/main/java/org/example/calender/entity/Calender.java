859d6c6a0aede4c3f3cd2d449a05fe94eb9bda39
csy10001 <fuitshine@gmail.com>
2025-08-01 10:38:41 +0900

    일단 BaseEntity, @EnableJpaAuditing 만들었습니다.

---
+++ src/main/java/org/example/calender/entity/Calender.java
@@ -0,0 +1,26 @@
+package org.example.calender.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Entity
+@RequiredArgsConstructor
+public class Calender extends BaseEntity{
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+
+    public Calender(String content) {
+        this.content = content;
+    }
+
+    public void update(String content) {
+        this.content = content;
+    }
+}
